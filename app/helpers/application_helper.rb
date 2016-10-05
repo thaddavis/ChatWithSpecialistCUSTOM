64e208dc4227309b723ae3f54b91eac07fb35d1a
thaddavis <thaddadavis@gmail.com>
2016-10-05 03:27:05 -0400

    Asdf

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,20 +25,15 @@ module ApplicationHelper
   def ConnectedUsersForDisplay(users)
     returnArray = []
 
-
-
     users.each do |i|
       user_hash = Hash.new
       user = User.find_by_email(i)
 
-      debugger
       user_hash[:email] = user.email
       user_hash[:last_sign_in_at] = user.last_sign_in_at
       user_hash[:status] = ConnectedList.retrieve_status(user.email)
 
       returnArray << user_hash
-
-
 
     end
     returnArray
@@ -49,8 +44,6 @@ module ApplicationHelper
     if current_user && current_user.timedout?(current_user.current_sign_in_at)
       sign_out(current_user)
     end
-
-    debugger
 
     ActionCable.server.broadcast "appearances_channel",
                                message_type: "subscribed",
