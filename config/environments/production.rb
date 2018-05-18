36d2074ea6b323d6373776e41549100a5d813207
Tad <t@t.com>
2018-05-17 23:32:09 -0400

    fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,8 +37,8 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.web_socket_server_url = "wss://pure-thicket-94498.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = [ 'http://pure-thicket-94498.herokuapp.com', 'https://pure-thicket-94498.herokuapp.com' ]
+  config.web_socket_server_url = "wss://fierce-dawn-59409.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = [ 'http://fierce-dawn-59409.herokuapp.com', 'https://fierce-dawn-59409.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
